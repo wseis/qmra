05e7216e7367598e69ef0919d1506c7be475cc8c
wseis <wolfgang.seis@posteo.de>
2021-03-08 20:31:32 +0100

    add comparison

--- tools/qmratool/views.py
+++ tools/qmratool/views.py
@@ -40,7 +40,58 @@ def bayes(request):
 def comparison(request):
     user  = request.user
     if request.method == "POST":
-        return HttpResponse("Post received")
+        form = ComparisonForm(user, request.POST)
+        if form.is_valid():
+            comparison = Comparison()
+            comparison.save()
+            comparison.risk_assessment.set(form.cleaned_data["risk_assessment"])
+            results = []
+            ras = comparison.risk_assessment.all()
+            for i in range(len(ras)):
+                sim = simulate_risk(ras[i])
+                sim["Assessment"] = ras[i].name
+                results.append(sim)
+                
+            df = pd.concat(results)
+
+            fig = px.box(df, x="variable", y="value", color="Assessment", 
+                            points="all",  log_y =True, 
+                            title="Risk as probability of infection per year",
+                            color_discrete_sequence=["#005269", "#007c9e", "#a3d1ec","#3494ae","#00B8eb"])
+
+
+            fig.update_layout(
+                font_family="Helvetica Neue, Helvetica, Arial, sans-serif",
+                font_color="black",
+                title = {'text':'Risk assessment as probability of infection per year'},
+                xaxis_title = "Reference Pathogen",
+                yaxis_title = "Probability of infection per year",
+                #markersize= 12,
+                )
+
+            fig.update_layout(legend=dict(
+                            orientation="h",
+                            yanchor="top",
+                            y=-.2,
+                            xanchor="left",
+                            x=0,
+                               font=dict(
+                                family="Courier",
+                                size=14,
+                                color="black"
+                            ),
+                            bgcolor="white",
+                            bordercolor="#007c9e",
+                            borderwidth=0))
+
+
+            fig.update_traces(marker_size = 8)#['#75c3ff', "red"],#, marker_line_color='#212c52',
+
+
+            risk_plot = plot(fig, output_type = "div")
+
+
+            return render(request, "qmratool/results.html",  {"risk_plot": risk_plot})
     else:
         form = ComparisonForm(user=user)
     return render(request, 'qmratool/comparison.html', {"form":form})
@@ -459,3 +510,59 @@ def api_exposure_by_id(request, exposure_id):
     
     # Return emails in reverse chronologial order
     return JsonResponse([exposure.serialize() for exposure in exposures], safe=False)
+
+
+def annual_risk(nexposure, event_probs):
+    return 1-np.prod(1-np.random.choice(event_probs, nexposure, True))
+
+
+def simulate_risk(ra):
+    # Selecting inflow concentration based in source water type
+    df_inflow = read_frame(Inflow.objects.filter(water_source=ra.source).values("min", "max", "pathogen__pathogen", "water_source__water_source_name"))
+    df_inflow = df_inflow[df_inflow["pathogen__pathogen"].isin(["Rotavirus", "Campylobacter jejuni", "Cryptosporidium parvum"])]
+    # Querying dose response parameters based on pathogen inflow
+    dr_models = read_frame(DoseResponse.objects.filter(pathogen__in=Pathogen.objects.filter(pathogen__in=df_inflow["pathogen__pathogen"])))
+
+    # Querying for Logremoval based on selected treatments
+    df_treatment=read_frame(LogRemoval.objects.filter(treatment__in=ra.treatment.all()).values("min", "max", "treatment__name", "pathogen_group__pathogen_group"))
+    
+    # Summarizing treatment to treatment max and treatment min
+    df_treatment_summary=df_treatment.groupby(["pathogen_group__pathogen_group"]).sum().reset_index()
+
+    results = pd.DataFrame()
+
+    for index, row in df_inflow.iterrows():
+        d = df_inflow.loc[df_inflow["pathogen__pathogen"] ==row["pathogen__pathogen"]]
+        dr = dr_models.loc[dr_models["pathogen"]==row["pathogen__pathogen"]]
+
+        if row["pathogen__pathogen"] == "Rotavirus":
+            selector = "Viruses" 
+        elif row["pathogen__pathogen"]== "Cryptosporidium parvum":
+            selector = "Protozoa"
+        else:
+            selector = "Bacteria"
+        #result.append(selector)
+
+        df_treat = df_treatment_summary[df_treatment_summary["pathogen_group__pathogen_group"]==selector]
+
+
+
+        risk_df = pd.DataFrame({"inflow": np.random.normal(loc=(np.log10(float(d["min"])+10**(-8))+np.log10(float(d["max"]) ))/2, 
+                                                            scale = (np.log10(float(d["max"]))-np.log10(float(d["min"])+10**(-8) ))/4,  
+                                                            size = 10000),
+                                "LRV": np.random.uniform(low= df_treat["min"], 
+                                                         high= df_treat["max"], 
+                                                         size= 10000) })
+        risk_df["outflow"]=risk_df["inflow"] - risk_df["LRV"]
+        risk_df["dose"] = (10**risk_df["outflow"])*float(ra.exposure.volume_per_event)
+
+        if selector != "Protozoa":
+            risk_df["probs"] = 1 - (1 + (risk_df["dose"]) * (2 ** (1/float(dr.alpha)) - 1)/float(dr.n50)) ** -float(dr.alpha)
+        else:
+            risk_df["probs"] = 1 - np.exp(-float(dr.k)*(risk_df["dose"]))
+
+        results[row["pathogen__pathogen"]] = [annual_risk(int(ra.exposure.events_per_year), risk_df["probs"] ) for _ in range(1000)]
+
+    results_long = pd.melt(results)
+    results_long["log probability"] = np.log10(results_long["value"])
+    return results_long
