6173a7effccad0d75a7270eaa199f5e207a8ae3c
wseis <wolfgang.seis@posteo.de>
2021-08-08 10:49:21 +0200

    commit

--- tools/tools/settings.py
+++ tools/tools/settings.py
@@ -85,7 +85,7 @@ AUTH_USER_MODEL = 'qmratool.User'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'qmra.db',
     }
 }
 
