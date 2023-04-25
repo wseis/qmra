34a7b7cbcc045f362d7d91feb1764850916b9a94
wseis <wolfgang.seis@posteo.de>
2023-04-25 05:08:00 +0200

    update comparison style

--- tools/qmratool/helper_functions.py
+++ tools/qmratool/helper_functions.py
@@ -11,7 +11,7 @@ def plot_comparison(df, df2):
                      "pathogen": "Reference pathogen"
                      },
                     title="Risk as probability of infection per year",
-                    color_discrete_sequence=["#004254", "#007c9e", "#a3d1ec","#3494ae","#00B8eb"])
+                    color_discrete_sequence=["#78BEF9", "#8081F1",  "#5F60B3","#3D3E73", "#F29C99", "#7375D9", "#CBCCF4"])
 
     fig.add_traces(
     list(px.box(df2, x="Assessment", y="value", 
@@ -22,7 +22,7 @@ def plot_comparison(df, df2):
                    #  "pathogen": "Reference pathogen"
                    #  },
                     title="Risk as probability of infection per year",
-                    color_discrete_sequence=["#00566E", "#00566E", "#00566E","#00566E","#00566E"]).select_traces()
+                    color_discrete_sequence=["#7375D9", "#7375D9", "#7375D9", "#7375D9"]).select_traces()
     ))
 
 
@@ -34,7 +34,7 @@ def plot_comparison(df, df2):
         yaxis_title = "Probability of infection per year",
         annotations=[go.Annotation(y = -4, x =1.2,
                 text = "Tolerable risk level of 1/10000 infections pppy",
-                bgcolor = "#007c9f",
+                bgcolor = '#0003e2',
                 bordercolor= "white",
                 borderpad = 5,
                 font = dict(color = "white"))]
@@ -58,7 +58,7 @@ def plot_comparison(df, df2):
                     borderwidth=0))
 
 
-    fig.add_hline(y=0.0001, line_dash="dashdot", line=dict(color="#007c9f", width = 3),
+    fig.add_hline(y=0.0001, line_dash="dashdot", line=dict(color="#0003e2", width = 3),
 
      )
 
